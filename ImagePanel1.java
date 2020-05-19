4d7171c97090a10aa2c372652eeb3d50c7155480
Sofia Echavarria <21Echavarria@flintridgeprep.org>
2020-05-19 13:12:31 -0700

    I added the rings function as an option and added moons that will all be different colors around the planet

--- ImagePanel1.java
+++ ImagePanel1.java
@@ -6,8 +6,11 @@ public class ImagePanel1 extends JPanel
     ArrayList<Image> imageList = new ArrayList<Image>();
     ArrayList<Integer> positionxList = new ArrayList<Integer>();
     ArrayList<Integer> positionyList = new ArrayList<Integer>();
-    ArrayList<Color> colorList = new ArrayList<Color>();
     Color randomColor;
+    ImageIcon rings = new ImageIcon("rings1.png");
+    boolean ring = false;
+    int moons = 0;
+    int mass = 230;
     
     public void addColor(int bluecolor, int greencolor, int redcolor){
         int blue = (int)(bluecolor);
@@ -17,11 +20,49 @@ public class ImagePanel1 extends JPanel
         //colorList.add(randomColor);
     }
     
+    public void addRings(boolean answer){
+        if(answer == true){
+            ring = true;
+        }else{
+            ring = false;
+        }
+    }
+    
+    public void addMoons(int amount){
+        if(amount == 0){
+            moons = 0;
+        }else if(amount == 2){
+            moons = 2;
+        }else if(amount == 5){
+            moons = 5;
+        }
+    }
+    
+    public Color chooseColor(){
+        int blue = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int red = (int) (Math.random() * 255);
+        Color randomColor1 = new Color(red, green, blue);
+        return randomColor1;
+    }
+    
     @Override 
     public void paintComponent(Graphics g){
         //g.fillRect(0,0, 400, 400);
+        
         g.setColor(randomColor);
-        g.fillOval(100, 100, 200, 200);
+        g.fillOval(300, 80, 230, 230);
+        if(ring == true){
+            Image image = rings.getImage();
+            g.drawImage(image, 100, 140, this);
+        }
+        
+        for(int i = 0; i < moons; i++){
+            g.setColor(chooseColor());
+            int positionx = (int)(Math.random() * 600);
+            int positiony = (int)(Math.random() * 300);
+            g.fillOval(positionx, positiony, 50, 50);
+        }
         
     }
 }
